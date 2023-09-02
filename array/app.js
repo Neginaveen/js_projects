ce30fb83d22f7d3b6912dc833b4e74fd06dd87c0
Neginaveen <nnegi9112000@gmail.com>
2023-09-02 14:36:28 +0530

    added javascript timer

---
+++ array/app.js
@@ -0,0 +1,29 @@
+
+// ---------push-----------
+
+
+// let arr = [10, 20, 30, 40]
+// arr.push(50, 60)
+// console.log(arr)
+
+
+// -----pop--------
+
+// let arr = [10, 20, 30, 40]
+// arr.pop()
+// console.log(arr)
+
+// --------shift--------
+
+
+// let arr= [10, 20 ,30 ,40]
+// arr.shift()
+// console.log(arr)
+
+
+// -----unshift--------
+
+
+// let arr=[10,20,30,40]
+// arr.unshift(100,200)
+// console.log(arr)
